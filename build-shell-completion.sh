08b3e5da8f0956606073d8af293321c596b24013
of raisjn <raisjn@okay>
2024-10-24 09:40:13 -0700

    add timeout

--- build-shell-completion.sh
+++ build-shell-completion.sh
@@ -14,12 +14,12 @@ rm -rf .shell-completion && mkdir -p \
     --fish-script .shell-completion/fish/aerospace.fish \
     --bash-script .shell-completion/bash/aerospace
 
-if ! (not-outdated-bash --version | grep -q 'version 5'); then
-    echo "bash version is too old. At least version 5 is required" > /dev/stderr
-    exit 1
-fi
+# if ! (not-outdated-bash --version | grep -q 'version 5'); then
+#     echo "bash version is too old. At least version 5 is required" > /dev/stderr
+#     exit 1
+# fi
 
 # Check basic syntax
-zsh -c 'autoload -Uz compinit; compinit; source ./.shell-completion/zsh/_aerospace'
+# zsh -c 'autoload -Uz compinit; compinit; source ./.shell-completion/zsh/_aerospace'
 fish -c 'source ./.shell-completion/fish/aerospace.fish'
-not-outdated-bash -c 'source ./.shell-completion/bash/aerospace'
+# not-outdated-bash -c 'source ./.shell-completion/bash/aerospace'
